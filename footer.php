a11ce74f38f632fec6d7fcf9de0834ed8f92e3c5
mikhsan21 <77140168+mikhsan21@users.noreply.github.com>
2023-04-12 04:31:12 +0700

    Menambahkan File Footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Muhammad Ikhsan Nugraha</span></strong>. All Rights Reserved
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
