7a3357b1b18d4e279e83e0510ab63abeb02ba27f
mikhsan21 <77140168+mikhsan21@users.noreply.github.com>
2023-04-12 04:27:42 +0700

    Menambah File About.php

---
+++ about.php
@@ -0,0 +1,64 @@
+<!-- ======= About Section ======= -->
+<section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>
+            Saya Mahasiswa Dari Universitas Amikom Purwokerto, Program Studi Ilmu Komputer Semester 6. Saat ini Saya Mengikuti Program Kampus Merdeka Yaitu Studi Independen di PT. Nurul Fikri Cipta Inovasi (NF Computer) Dengan Fokus Pembelajaran Pada Fullstack Web Developer.
+          </p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4">
+            <img src="assets/img/ikhsan.jpg" class="img-fluid" width="150" height="50" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>Biodata Diri</h3>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Nama Lengkap :</strong> Muhammad Ikhsan Nugraha</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>TTL :</strong> Purwokerto, 21 Mei 2001</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Jenis Kelamin :</strong> Laki-Laki</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>City :</strong> Purwokerto</li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Kampus :</strong>Universitas Amikom Purwokerto</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Age :</strong> 21 Tahun</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Email :</strong> mikhsannugraha39@gmail.com</li>
+                  <li><i class="bi bi-rounded-right"></i> <strong>Handphone :</strong> +62 82137932126</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br/>
+        <div class="skill-mf">
+                    <p class="title-s">Skill</p>
+                    <span>HTML</span> <span class="pull-right">80%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <br>
+                    <span>CSS3</span> <span class="pull-right">70%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <br>
+                    <span>PHP</span> <span class="pull-right">50%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <br>
+                    <span>JAVASCRIPT</span> <span class="pull-right">75%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                </div>
+
+      </div>
+    </section><!-- End About Section -->
