385d818af08d4a26e11022ebd8fbd0ffe6a5a709
mikhsan21 <77140168+mikhsan21@users.noreply.github.com>
2023-04-12 04:42:13 +0700

    Menambahkan File Resume.php

---
+++ resume.php
@@ -0,0 +1,48 @@
+ <!-- ======= Resume Section ======= -->
+ <section id="resume" class="resume">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <h3 class="resume-title">Ringkasan</h3>
+            <div class="resume-item pb-0">
+              <h4>Tujuan Karir</h4>
+              <p><em>Mahasiswa Aktif Ilmu Komputer Universitas Amikom Purwokerto Yang Memiliki Passion di Bidang Web Programming Dalam Membuat Website Dengan Menggunakan Bahasa Pemrograman, Ingin Memiliki Passion di Bidang Jaringan Komputer dan Mengikuti Perkembangan Teknologi Baru.</em></p>
+              
+            </div>
+
+            <h3 class="resume-title">Pendidikan</h3>
+            <div class="resume-item">
+              <h4>MAN 1 Banyumas</h4>
+              <h5>2017-2020</h5>
+              <p><em>Ilmu Pengetahuan Sosial</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>Bachelor Degree in Computer Science</h4>
+              <h5>2020 - 2024</h5>
+              <p><em>Universitas Amikom Purwokerto</em></p>
+              <p>Mahasiswa Aktif Program Studi Informatika Universitas Amikom Purwokerto Semester 6.</p>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <h3 class="resume-title">Pengalaman Profesional</h3>
+            <div class="resume-item">
+              <h4>Kampus Mengajar Angkatan 4</h4>
+              <h5>Agustus - Desember 2022</h5>
+              <p><em>SD Negeri 2 Purwokerto Wetan </em></p>
+              <p>
+              <ul>
+                <li>Membantu Bapak/Ibu Guru Dalam Kegiatan Pembelajaran Di Dalam dan Di Luar Kelas.</li>
+                <li>Membantu Bapak/Ibu Guru dan Siswa-Siswi Dalam Adaptasi Teknologi, Membantu Meningkatkan Literasi dan Numerasi dan Administrasi Sekolah.</li>
+              </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
