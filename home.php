2b3f5fe27dc99be492468bd6278a7dc6b7a22a82
mikhsan21 <77140168+mikhsan21@users.noreply.github.com>
2023-04-12 04:40:00 +0700

    Menambahkan File Home.php

---
+++ home.php
@@ -0,0 +1,8 @@
+ <!-- ======= Hero Section ======= -->
+ <section id="hero" class="d-flex align-items-center">
+    <div class="container d-flex flex-column align-items-center" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Muhammad Ikhsan Nugraha</h1>
+      <h2>I'm a MSIB Batch 4 Akademi Fullstack Web Developer</h2>
+     
+    </div>
+  </section><!-- End Hero -->
